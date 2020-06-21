5161b3daef036e46f37da11c35f2c61b132907cd
rajputlaxmisk <rajputlaxmisk.com>
2020-06-21 17:32:13 +0530

    Entity class ans repository interface implemented

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="studenttab")
+public class Student 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name="sname")
+	private String name;
+	@Column(name="scourse")
+	private String course;
+	@Column(name="sfee")
+	private double fee;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", course=" + course + ", fees=" + fee + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public double getFees() {
+		return fee;
+	}
+	public void setFees(double fees) {
+		this.fee = fees;
+	}
+	
+	
+	
+}
