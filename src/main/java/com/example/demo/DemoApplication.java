75581fda736ffb0d85ec22beb4b8ac3954759e70
rajputlaxmisk <rajputlaxmisk@gmail.com>
2020-08-12 17:57:20 +0530

    commit webhook

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
  
@@ -18,6 +22,13 @@ public class DemoApplication {
 		
 		return "Hello World";
 	}
+	
+	@PostMapping("/webhook")
+    public void getMyJson(@RequestBody Map<String, Object> json) {
+    System.out.println("WebHook collected JSON: " + json);
+    }
+
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
